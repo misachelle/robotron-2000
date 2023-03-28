b4b7d490d4c0cc99bb9a7b133786786f952585d1
Michelle Saez <michellesaez1@hotmail.com>
2023-03-28 15:42:23 -0300

    exercício aula 01

---
+++ 01 - exercicio aula 01/main.js
@@ -0,0 +1,7 @@
+//Exercício:
+//  Praticar é fundamental para fixar o conhecimento adquirido, para isso, temos o primeiro desafio! Utilizando JavaScript, substitua a frase dentro das tags <p> </p> do projeto
+
+
+// comandos do console:
+document.querySelector('#texto').innerText = "O inverno eh uma merda";
+
